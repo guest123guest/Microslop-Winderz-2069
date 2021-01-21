c18eb4edba13198ab2de4bc16f82766dcd5086e1
DESKTOPGOOOSE <77574002+DESKTOPGOOOSE@users.noreply.github.com>
2021-01-21 13:53:37 -0500

    Update boot.asm

--- boot.asm
+++ boot.asm
@@ -1,22 +1,22 @@
-// Windows 11
+// Winderz 2069
 findcpu Intel X96
 or
 findcpu AMD Ryan
 or else
-print "UNSUPPORTED CPU"
+print "That CPU is too old and retarded to fit!"
 then
-include "c:\windows\system32\bootsound.asm"
+include "c:\winderz\system32\bootsound.asm"
 iffound
-include "c:\windows\asm\sound.asm"
+include "c:\winderz\asm\sound.asm"
 iffound
-include "c:\windows\winvercode.asm"
+include "c:\winderz\winvercode.asm"
 iffound
 start "c:\windows\kernel.exe"
 start "c:\windows\system.exe"
 start "c:\autoexec.bat"
 
-// Windows 11
+// Winderz 2069
 
 If version = "COREOS"
 
-start "c:\windows\system32\command.com"
+start "c:\winderz\system32\command.com"
